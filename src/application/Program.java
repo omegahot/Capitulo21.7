32b92efa53c66d475acb80a1adac7eaf9feda61f
omegahot <leo_omegahot@hotmail.com>
2022-03-30 21:07:52 -0300

    Criando classe Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department obj = new Department(1, "Livros");
-		System.out.println(obj);
+		Seller sel = new Seller(21, "Bob", "bob@email.com", new Date(), 3000.0, obj);
+		
+		System.out.println(sel);
 	}
 
 }
